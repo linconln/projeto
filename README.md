32f8b229c58e880d32f83c08d47d92863da4f769
Linconln Tadao Yamachita <linconln@gmail.com>
2024-06-16 22:04:48 -0300

    token do eks

--- README.md
+++ README.md
@@ -2,5 +2,10 @@ Criação da infraestrutura usando github Actions e Terraform
 
 Executar em Actions, create-eks.yaml
 Os arquivos na pasta Terraform provem da documentação em:
+
+Criar infra do EKS
 https://developer.hashicorp.com/terraform/tutorials/kubernetes/eks
 https://developer.hashicorp.com/terraform/tutorials/kubernetes/kubernetes-provider?variants=kubernetes%3Aeks
+
+Usar s3 tfstate
+https://developer.hashicorp.com/terraform/language/settings/backends/s3
--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -12,9 +12,10 @@ data "aws_eks_cluster" "cluster" {
   name = data.terraform_remote_state.eks.outputs.cluster_name
 }
 
-provider "kubernetes" {
+provider "kubectl" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+#  token                  = data.aws_eks_cluster_auth.main.token
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
@@ -25,12 +26,6 @@ provider "kubernetes" {
       data.aws_eks_cluster.cluster.name
     ]
   }
-}
-
-provider "kubectl" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.main.token
   load_config_file       = false
 }
 
