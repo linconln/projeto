58ad21fe489f4f635ee24127a1b859c241ba0b12
Linconln Tadao Yamachita <linconln@gmail.com>
2024-06-16 22:36:16 -0300

    kubectl path

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -3,7 +3,7 @@ data "terraform_remote_state" "eks" {
 
   config = {
     bucket = "linconln-devops"
-    key = "devops"
+    key    = "devops"
     region = var.aws_region
   }
 }
@@ -15,7 +15,7 @@ data "aws_eks_cluster" "cluster" {
 provider "kubectl" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-#  token                  = data.aws_eks_cluster_auth.main.token
+  #  token                  = data.aws_eks_cluster_auth.main.token
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
@@ -26,14 +26,14 @@ provider "kubectl" {
       data.aws_eks_cluster.cluster.name
     ]
   }
-  load_config_file       = false
+  load_config_file = false
 }
 
-data "kubectl_filename_list" "docs" {
-  pattern = "../kubernetes/*.yaml"
+data "kubectl_path_documents" "docs" {
+    pattern = "../kubernetes/*.yaml"
 }
 
 resource "kubectl_manifest" "test" {
-  for_each  = data.kubectl_file_documents.docs.manifests
-  yaml_body = each.value
+    for_each  = toset(data.kubectl_path_documents.docs.documents)
+    yaml_body = each.value
 }
